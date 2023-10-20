31a8295379577278ef7ffb41f010fbc7423f2f15
Elmagonza <elmagonz4.dev@gmail.com>
2023-10-20 00:48:11 -0300

    Cambio footer de erika

--- js/headerandfooter.js
+++ js/headerandfooter.js
@@ -1,13 +1,13 @@
 // Define el contenido del encabezado
 let headerContenido = `
     <a href=""><img class="logo" src="img/logo.jpg" alt="logo" width="35em" > </a>
-    <nav class="navbar-grid"> 
+    <nav class="navbar-grid">
         <ul class="menu">
             <li><a href="index.html" class="item">Inicio</a></li>
             <li><a href="nosotros.html" class="item">Nosotros</a></li>
             <li><a href="galeria.html" class="item">Galeria</a></li>
             <li><a href="contacto.html" class="item">Contacto</a></li>
-        </ul> 
+        </ul>
     </nav>
 `;
 
@@ -23,36 +23,36 @@ function agregarHeader() {
 agregarHeader();
 
 let footerContenido = `
- <div>
-    <h4>Grupo Scout Nº7-Sagrado Corazón </h4>
-    <br>
-    <p>&copy; 2023-Desarrollado por alumnos de Codo a Codo </p>
-</div>
-<div>
-    <h4>Contacto</h4>
-    <ul>
-    <li>Tel:xx xxxx-xxxx</li>
-    <li>Dirección:Av. Gral. Tomás de Iriarte 3051</li>
-    <li>Gmail:xxxxxxx@gmail.com</li>
-    </ul>
-</div>
-<div class="redes">  <h4>Nuestras redes</h4>
-    <a href="https://www.facebook.com/sagradocorazonbarracas" target="_blank"><img src="img/redes/facebook.png"
-            alt="Facebook" width="20em" title="Ir a Facebook"></a>
-    <a href="https://www.instagram.com/scoutsagrado/" target="_blank"><img src="img/redes/instagram.png"
-            alt="Instagram" width="20em" title="Ir a Instagram"></a>
-    <a href="https://twitter.com/ScoutsSagrado" target="_blank"><img src="img/redes/twitter.png" alt=""
-            width="20em" title="Ir a Twitter"></a>
-</div>
+
+    <div class="box">
+        <div class="esc">
+            <h1>Grupo Scout Nº7-Sagrado Corazón </h1>
+        </div>
+    </div>
+    <div class="box">
+        <h2>Nuestras redes</h2>
+        <div class="redes-soc">
+            <a href="https://www.facebook.com/sagradocorazonbarracas" target="_blank"><img
+                    src="img/redes/facebook.png" alt="Facebook" width="20em" title="Ir a Facebook"></a>
+            <a href="https://www.instagram.com/scoutsagrado/" target="_blank"><img src="img/redes/instagram.png"
+                    alt="Instagram" width="20em" title="Ir a Instagram"></a>
+            <a href="https://twitter.com/ScoutsSagrado" target="_blank"><img src="img/redes/twitter.png" alt=""
+                    width="20em" title="Ir a Twitter"></a>
+        </div>
+    </div>
+    <div class="grupo2">
+        <p>&copy; Derechos reservado 2023<br>Desarrollado por alumnos de Codo a Codo </p>
+    </div>
+
 `;
 
 // Función para agregar el footer a todas las páginas
 function agregarFooter() {
     let footer = document.createElement('footer');
-    footer.id = "footer";
+    footer.id = "pie";
     footer.className = 'footer';
     footer.innerHTML = footerContenido;
     document.body.appendChild(footer);
 }
-    
+
 agregarFooter();
